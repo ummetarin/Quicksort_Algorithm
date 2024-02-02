f3344e1a97f492b1d4763d715e011c41c3a480ea
ummetarin <ummehomairatarin@gmail.com>
2024-02-02 16:13:46 +0600

    added partition function

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,6 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int partition(int arr[],int l,int r){
+    int pivot=arr[r];
+    int i=l,j=r-1;
+    while(pivot==arr[r]){
+    
+     while(arr[i]<pivot){
+        i++;
+     }
+     while(arr[j]>pivot){
+        j--;
+     }
+     if(i<j){
+        swap(arr[i],arr[j]);
+     }
+     else{
+        swap(arr[i],arr[r]);
+        return i;
+     }
+
+
+    }
+}
+
+
 int main(){
 
 
