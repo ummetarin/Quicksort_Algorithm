ef4cd439d427ea94c806e8bf4fb522ad22d82fd9
ummetarin <ummehomairatarin@gmail.com>
2024-02-02 16:16:50 +0600

    added quicksort main function

--- quicksort.cpp
+++ quicksort.cpp
@@ -19,8 +19,14 @@ int partition(int arr[],int l,int r){
         swap(arr[i],arr[r]);
         return i;
      }
+   }
+}
 
-
+void quicksort(int arr[],int l,int r){
+    if(l<r){
+        int div=partition(arr,l,r);
+        quicksort(arr,l,div-1);
+        quicksort(arr,div+1,r);
     }
 }
 
